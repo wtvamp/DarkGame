d766dc023e6fd1ed243fc7ce2543d636610d342e
acruz <acruz@vailsys.com>
2020-10-07 16:08:41 -0500

    added methods and constuctors
    Co-authored-by: mmadrigal@vailsys.com <mmadrigal@vailsys.com>

---
+++ src/gamesupport/SceneGenerator.java
@@ -0,0 +1,35 @@
+package gamesupport;
+
+public class SceneGenerator {
+    private String storyLine;
+    private Encounter monsterEncount; 
+    private SuperHero hero; 
+    private String choiceQuestion; 
+    private String choiceAnswer; 
+    private SuperHero sidekick; 
+    private Monster enemy; 
+
+    public SceneGenerator(String storyLineFromApp, SuperHero heroFromApp, SuperHero sidekickFromApp, String choiceQuestionFromApp, String choiceAnswerFromApp, Monster enemyFromApp) {
+
+    }
+
+    public SceneGenerator(String storyLineFromApp, String choiceQuestionFromApp, String choiceAnswerFromApp) {
+
+    }
+
+    public void printStoryline() {
+
+    }
+    
+    public String makeChoice() {
+        return "";
+    }
+    
+    public void startEncounter() {
+        
+    }
+}
+
+
+
+
