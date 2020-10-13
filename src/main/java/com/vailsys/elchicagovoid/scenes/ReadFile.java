5c5ec0bb410c78af45dd3942b7e4bf8f879d36b4
Warren Thompson <warrenjthompson.com>
2020-10-13 13:13:14 -0700

    fixed test

--- src/main/java/com/vailsys/elchicagovoid/scenes/ReadFile.java
+++ src/main/java/com/vailsys/elchicagovoid/scenes/ReadFile.java
@@ -5,19 +5,19 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class ReadFile {
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("src/main/java/com/vailsys/elchicagovoid/scenes/scenes1.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
 
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+  public void LoadFileFromText() {
+    try {
+        File myObj = new File("src/main/java/com/vailsys/elchicagovoid/scenes/scenes1.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
-  }
 }
---
+++ src/test/java/com/vailsys/elchicagovoid/gamesupport/ReadFileTest.java
@@ -0,0 +1,18 @@
+package com.vailsys.elchicagovoid.gamesupport;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.vailsys.elchicagovoid.scenes.ReadFile;
+
+public class ReadFileTest {
+
+    @Test
+    void testLoadFile() {
+        ReadFile readFile = new ReadFile();
+        readFile.LoadFileFromText();
+        assertEquals(1, 1);
+    }
+    
+}
