5c1e3231e3bb414c496a25d636f2e0b863cf80d9
Warren Thompson <warrenjthompson.com>
2020-10-13 12:59:23 -0700

    Fixed up some spells
    Co-authored-by: aramirez@vailsys.com <aramirez@vailsys.com>
    Co-authored-by: jmorales@vailsys.com <jmorales@vailsys.com>
    Co-authored-by: acruz@vailsys.com <acruz@vailsys.com>

--- src/main/java/com/vailsys/elchicagovoid/gamesupport/magicspellfolder/SummonCreatureSpell.java
+++ src/main/java/com/vailsys/elchicagovoid/gamesupport/magicspellfolder/SummonCreatureSpell.java
@@ -1,22 +1,20 @@
 package com.vailsys.elchicagovoid.gamesupport.magicspellfolder;
 
-
-import java.util.Random;
-
 public class SummonCreatureSpell extends MagicSpell {
-        public static void main( String args[] ) {
-        //generate random values from 0-24
-        // num is the actual number that gets used
-            int minSummonSpell = 1;
-            int maxSummonSpell= 2;
-       //Generate random double value from 50 to 100 
-            int random_int = (int)(Math.random() * (maxSummonSpell - minSummonSpell + 1) + minSummonSpell);
-            System.out.println(random_int);
+    private Boolean dealtDamage;
 
-            if (random_int == 1){
-                this.MagicSpell.activateDamage(15);
-            } else {
-
+    public SummonCreatureSpell() {
+        int minSummonSpell = 1;
+        int maxSummonSpell= 2;
+        
+        int random_int = (int)(Math.random() * (maxSummonSpell - minSummonSpell + 1) + minSummonSpell);
+        System.out.println(random_int);// take this out alter 
+        if (random_int == 1) {
+            this.dealtDamage = super.activateDamage(15);
+        } else {
+            //
         }
     }
+}
+           
     
