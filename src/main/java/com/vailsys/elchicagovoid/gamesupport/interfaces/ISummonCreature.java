edf8c1491ebd7536d84bb56cea65b684091ca6d3
Warren Thompson <warrenjthompson.com>
2020-10-15 13:48:47 -0700

    Other interfaces added.

--- src/main/java/com/vailsys/elchicagovoid/gamesupport/interfaces/ISummonCreature.java
+++ src/main/java/com/vailsys/elchicagovoid/gamesupport/interfaces/ISummonCreature.java
@@ -1,3 +1,6 @@
+package com.vailsys.elchicagovoid.gamesupport.interfaces;
+import com.vailsys.elchicagovoid.gamesupport.SuperHero;
+
 public interface ISummonCreature {
-    
+    public void summonCreature(SuperHero heroSummon);
 }
