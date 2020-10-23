0f11057cd7a07adae470a527c8802b52dd8d90b6
Warren Thompson <warrenjthompson@gmail.com>
2020-10-23 10:37:02 -0700

    Fixed actual test

--- src/test/java/com/vailsys/elchicagovoid/gamesupport/VehicleTest.java
+++ src/test/java/com/vailsys/elchicagovoid/gamesupport/VehicleTest.java
@@ -1,63 +1,57 @@
-// package com.vailsys.elchicagovoid.gamesupport;
+package com.vailsys.elchicagovoid.gamesupport;
 
-// import com.vailsys.elchicagovoid.gamesupport.interfaces.IVehicle;
+import com.vailsys.elchicagovoid.gamesupport.interfaces.IVehicle;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import java.io.*;
+import java.io.*;
 
-// import org.junit.jupiter.api.BeforeEach;
-// public class VehicleTest  {  
+import org.junit.jupiter.api.BeforeEach;
+
+public class VehicleTest  {  
     
-//     private Vehicle systemUnderTest;
+    private Vehicle systemUnderTest;
 
-//     @BeforeEach
-//     void assignVehicle() {
-//         this.systemUnderTest = new Vehicle(new SuperHero("IRS Taxman", new Weapon(8, 8, "Gun", "Knife"), new Weapon(3, 3, "Tax", "Deductibles"), 1000, "Bro",50));
-//     }
-//     void testVehicle(){
-//         System.out.println("");
-//         //assertEquals(expected, driver.characterName + " the " + driver.HeroName + " is driving the vehicle.");
-//        //assertNull(vehicleTesting.drive());
-//     }
+    @BeforeEach
+    void assignVehicle() {
+        this.systemUnderTest = new Vehicle(new SuperHero("IRS Taxman", new Weapon(8, 8, "Gun", "Knife"), new Weapon(3, 3, "Tax", "Deductibles"), 1000, "Bro",50));
+    }
+    void testVehicle(){
+        System.out.println("");
+    }
 
-//     @Test
-//     void Testdrive() {
-//         assertEquals(true, this.systemUnderTest.drive());
-//         assertEquals("Bro", this.systemUnderTest.driver.getCharacterName());
-   
-//     }
+    @Test
+    void Testdrive() {
+        assertEquals(true, this.systemUnderTest.drive());
+        assertEquals("Bro", this.systemUnderTest.driver.getCharacterName());
+    }
 
-//     // private void AssertEquals(String string, Object drive) {
-//     // }
-//     @Test
-//     void Testbrake() {
-//         assertEquals(false, this.systemUnderTest.brake());
-//         assertEquals("Bro", this.systemUnderTest.driver.getCharacterName());
-//     }
-//     @Test
-//     void Testturn() {
-//         // TODO Auto-generated method stub
-//         assertEquals(this.systemUnderTest.driver.getCharacterName() + " the " + this.systemUnderTest.getHeroName() + " did a Tokyo drift. Skeet skeet.", this.systemUnderTest.turn());
-//     }
+    @Test
+    void Testbrake() {
+        assertEquals(false, this.systemUnderTest.brake());
+        assertEquals("Bro", this.systemUnderTest.driver.getCharacterName());
+    }
+    @Test
+    void Testturn() {
+        // TODO Auto-generated method stub
+        assertEquals(this.systemUnderTest.driver.getCharacterName() + " the " + this.systemUnderTest.driver.getHeroName() + " did a Tokyo drift. Skeet skeet.", this.systemUnderTest.turn());
+    }
 
-//     private void assetrEquals(String string, Object turn) {
-//     }
 
-//     void Testhold(Object item) {
-//         // TODO Auto-generated method stub
+    void Testhold(Object item) {
+        // TODO Auto-generated method stub
 
-//     }
+    }
 
-//     void Testshield() {
-//         // TODO Auto-generated method stub
+    void Testshield() {
+        // TODO Auto-generated method stub
 
-//     }
+    }
 
-//     void Testcrash() {
-//         // TODO Auto-generated method stub
+    void Testcrash() {
+        // TODO Auto-generated method stub
 
-//     }
-// }
+    }
+}
