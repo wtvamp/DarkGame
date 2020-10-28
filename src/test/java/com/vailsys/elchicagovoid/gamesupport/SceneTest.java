e664cc1de0a5d7d692b3c3177169baa886934215
jmorales8 <jaimmoral98@gmail.com>
2020-10-28 12:30:39 -0500

    Made scene test

--- src/test/java/com/vailsys/elchicagovoid/gamesupport/SceneTest.java
+++ src/test/java/com/vailsys/elchicagovoid/gamesupport/SceneTest.java
@@ -5,17 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import java.util.ListResourceBundle;
+
+
 
 
 public class SceneTest {
     private Scene systemUnderTest;
-   @BeforeEach
-   
+    @BeforeEach
+
 @Test
-void Scenegenerator() {
+void Testscenegenerator() {
     assertEquals("", this.systemUnderTest.getTextDescription());
     assertEquals("", this.systemUnderTest.getChoicePrompt());
     assertEquals("", this.systemUnderTest.getEpilogue());
-    }
+}
+
+// @Test
+// String sceneTestFile() {
+//     assertEquals("", this.systemUnderTest.getChoicePrompt());
+//     return null;
+    
+//     }
 }
