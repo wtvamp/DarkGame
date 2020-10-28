eeefca50574770adaf4234b3f4f972865adf33d8
jmorales8 <jaimmoral98@gmail.com>
2020-10-28 12:56:42 -0500

    Co-authored-by: Alexander Cruz <acruz@vailsys.com> Co-authored-by: Angel Ramirez <aramirez@vailsys.com>

---
+++ src/test/java/com/vailsys/elchicagovoid/gamesupport/SpellsTest.java
@@ -0,0 +1,33 @@
+package com.vailsys.elchicagovoid.gamesupport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SpellsTest {
+    private Spells SUT;
+
+
+@BeforeEach
+
+@Test
+void spellsWarningMessage(){
+    assertEquals("", this.SUT.spellWarningMessage());
+}this.SSUT.sWarningMessagepellWarningMessage)
+public class SpellsTest {
+    
+}
+this.SSUT.sWarningMessagepellWarningMessageimport org.junit.jupiter.api.BeforeEach;
+
+
+public class SpellsTest {
+    
+}
+this.SSUT.sWarningMessagepellWarningMessageimport org.junit.jupiter.api.BeforeEach;
+
+()
+public class SpellsTest {
+    
+}
+this.SSUT.sWarningMessagepellWarningMessage
